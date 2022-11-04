fbf8c5acee679573a3a5f0b859fd9a28d2708674
SSeries <63185402+S-Series@users.noreply.github.com>
2022-11-04 10:10:56 +0900

    2022.11.04 || 10:10AM

--- NoteEditor/Assets/Script/LineOption.cs
+++ NoteEditor/Assets/Script/LineOption.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class LineOption : MonoBehaviour
 {
+    public Canvas canvas;
+    [SerializeField] SpriteRenderer[] noteRenderer;
     
+    public void ChangeLegnth(int _legnth)
+    {
+        if (_legnth == 0)
+        {
+            noteRenderer[1].enabled = false;
+            noteRenderer[2].enabled = false;
+        }
+    }
+    public void Selected(bool _isSelected)
+    {
+        foreach(SpriteRenderer renderer in noteRenderer)
+        {
+            renderer.enabled = _isSelected;
+        }
+    }
 }
