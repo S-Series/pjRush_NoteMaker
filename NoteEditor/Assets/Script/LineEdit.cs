65cc84c236a95bec8c4f6b52625e19181b3d74bd
SSeries <63185402+S-Series@users.noreply.github.com>
2022-11-29 11:47:17 +0900

    2022.11.29 || 11:47AM

--- NoteEditor/Assets/Script/LineEdit.cs
+++ NoteEditor/Assets/Script/LineEdit.cs
@@ -45,6 +45,7 @@ public class LineEdit : MonoBehaviour
         s_selectLineNote = null;
         s_lineEdit.selectLineOption = null;
         NoteClasses.EnableCollider(true);
+        SectorActivate(false);
     }
     public static void SelectLineNote(GameObject _object)
     {
@@ -121,8 +122,11 @@ public class LineEdit : MonoBehaviour
         inputFields[0].text = (s_selectLineNote.pos % 1600).ToString();
         inputFields[1].text = (Mathf.FloorToInt(s_selectLineNote.pos / 1600.0f)).ToString();
         inputFields[2].text = s_selectLineNote.startPower.ToString();
+        ActiveEndInput(s_selectLineNote.isSingle);
         if (!s_selectLineNote.isSingle) { inputFields[3].text = "---"; }
         else { inputFields[3].text = s_selectLineNote.endPower.ToString();}
+
+        toggle.isOn = s_selectLineNote.isSingle;
     }
 
     //* Sector Action
@@ -130,31 +134,36 @@ public class LineEdit : MonoBehaviour
     {
         if (s_selectLineNote == null) { return; }
         ChangeSingle();
-        toggle.isOn = s_selectLineNote.isSingle;
+        UpdateSectorInfo();
     }
     public void InputPos() //$ Along to inputFields[0]
     {
         if (s_selectLineNote == null) { return; }
 
         int _inputPos;
+        int _notePage;
+        _notePage = Mathf.FloorToInt(s_selectLineNote.pos / 1600.0f);
 
         try { _inputPos = Convert.ToInt32(inputFields[0].text); }
         catch { _inputPos = s_selectLineNote.pos; }
+        _inputPos = Mathf.Clamp(_inputPos, 0, 1600);
 
         s_selectLineNote.pos = _inputPos;
-        inputFields[0].text = _inputPos.ToString();
+        UpdateSectorInfo();
     }
     public void InputPage() //$ Along to inputFields[1]
     {
         if (s_selectLineNote == null) { return; }
 
-        int _inputPos;
+        int _inputPage;
+        int _notePos;
+        _notePos = Mathf.FloorToInt(s_selectLineNote.pos % 1600.0f);
 
-        try { _inputPos = Convert.ToInt32(inputFields[0].text); }
-        catch { _inputPos = s_selectLineNote.pos; }
+        try { _inputPage = Convert.ToInt32(inputFields[1].text); }
+        catch { _inputPage = s_selectLineNote.pos; }
 
-        s_selectLineNote.pos = _inputPos;
-        inputFields[0].text = _inputPos.ToString();
+        s_selectLineNote.pos = _inputPage;
+        UpdateSectorInfo();
     }
     public void InputPower(bool _isStart) //$ Along to inputFields[2] & inputFields[3]
     {
@@ -164,19 +173,20 @@ public class LineEdit : MonoBehaviour
 
         if (_isStart)
         {
-            try { _inputPower = Convert.ToInt32(inputFields[1].text); }
+            try { _inputPower = Convert.ToInt32(inputFields[2].text); }
             catch { _inputPower = s_selectLineNote.startPower; }
 
             inputFields[1].text = _inputPower.ToString();
         }
         else
         {
-            try { _inputPower = Convert.ToInt32(inputFields[2].text); }
+            try { _inputPower = Convert.ToInt32(inputFields[3].text); }
             catch { _inputPower = s_selectLineNote.endPower; }
 
             inputFields[2].text = _inputPower.ToString();
         }
 
         ChangePower(_isStart, _inputPower);
+        UpdateSectorInfo();
     }
 }
